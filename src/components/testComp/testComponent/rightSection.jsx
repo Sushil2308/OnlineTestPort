86baae26894c5279296acc986804d6d3593205cd
Sushil2308 <sushilprashaw23081998@gmail.com>
2022-01-23 17:13:46 +0530

    23-03-2022

--- src/components/testComp/testComponent/rightSection.jsx
+++ src/components/testComp/testComponent/rightSection.jsx
@@ -31,7 +31,7 @@ class RightSection extends Component{
          
            <div className="row row-cols-2 m-auto row justify-content-center align-items-center bg-shadow">
             {data.map((temp, index) => (
-              <div key={""+temp+index+1} onClick={()=>this.props.handleNext(temp.id)} className={temp.status===0?"bg-shadow col-2 rounded-circle bg-warning py-2 quesBack m-3 row justify-content-center align-items-center text-white fontSize":temp.status===1?"bg-shadow col-2 bg-primary py-2 rounded-circle quesBack m-3 row justify-content-center align-items-center text-white fontSize":"bg-shadow col-2 bg-success rounded-circle py-2 quesBack m-3 row justify-content-center align-items-center text-white fontSize"}>
+              <div key={""+temp+index+1} onClick={()=>this.props.handleNext(temp.id)} className={temp.status===0?"bg-shadow col-2 rounded-circle bg-warning py-2 quesBack m-3 row justify-content-center align-items-center text-white fontSize pointer":temp.status===1?"bg-shadow col-2 bg-primary py-2 rounded-circle quesBack m-3 row justify-content-center align-items-center text-white fontSize pointer":"bg-shadow col-2 bg-success rounded-circle py-2 quesBack m-3 row justify-content-center align-items-center text-white fontSize pointer"}>
                   <Tooltip title={temp.status===0?"Question Is Not Viewd":temp.status===1?"Question Is Already Viewd But Not Solved":"Question Is Solved"}><h5 className="row  justify-content-center align-items-center">{`Q.${index+1}`}</h5></Tooltip>
               </div>
             ))}
